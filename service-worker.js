5773c470f9bc45ae75f5152b3cc50a726ade7118
Jose Gilmar <jose.gilmar05@aluno.ifce.edu.br>
2025-06-19 07:27:24 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,29 @@
- 
+ const CACHE_NAME = "servicos-lugares-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/profissionais.html",
+  "/servicos.html",
+  "/lugares.html",
+  "/detalhe.html",
+  "/sobre.html",
+  "/contato.html",
+  "/css/styles.css",
+  "/js/main.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
