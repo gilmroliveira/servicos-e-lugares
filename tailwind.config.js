ed2ee5ccef78de42a2151fa42bfae216d4672eef
Jose Gilmar <jose.gilmar05@aluno.ifce.edu.br>
2025-06-19 12:22:33 -0300

    Atualizar o tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./*.html', './js/*.js'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        contrast: '#333',
+        light: '#f5f5f5',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
